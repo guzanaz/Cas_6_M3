b8508bb1d39f230aeee6b0afdab387a818846e12
Daniela <daniela.gallardo.reyes@gmail.com>
2021-05-05 10:35:25 +0200

    Update VolcatDeDades_a.java
    setting up del programa

--- volcatDeDades_a/volcatDeDades_a/VolcatDeDades_a.java
+++ volcatDeDades_a/volcatDeDades_a/VolcatDeDades_a.java
@@ -2,11 +2,18 @@
  * 
  */
 package volcatDeDades_a;
+import java.io.*;
 
 /**
- * @author Daniela
- *
+ * Cas_6 M03 Programa VolcatDeDades.java 
+ * Programa inicial del proyecto del mismo nombre. 
+ * Implementa una aplicación para la gestión de Ficheros utilizando el paquete java.io 
+ * 
+ * @author Daniela Gallardo Reyes
+ * @version 1.0 (pruebas iniciales)
+ * @since 05-05-2021
  */
+
 public class VolcatDeDades_a {
 
 	/**
