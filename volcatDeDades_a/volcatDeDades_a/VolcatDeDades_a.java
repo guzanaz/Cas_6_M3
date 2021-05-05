0dbfd9b4490220e7a5e2f7e4831e3e468bd57885
Daniela <daniela.gallardo.reyes@gmail.com>
2021-05-05 10:21:59 +0200

    update

---
+++ volcatDeDades_a/volcatDeDades_a/VolcatDeDades_a.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package volcatDeDades_a;
+
+/**
+ * @author Daniela
+ *
+ */
+public class VolcatDeDades_a {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
