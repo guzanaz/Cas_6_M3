54af3a5882462585aa2149299919727af9fe3987
Daniela <daniela.gallardo.reyes@gmail.com>
2021-05-17 17:20:37 +0200

    update
    avance

---
+++ Prueba/src/vDades/ProcessarFitxers.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package vDades;
+
+/**
+ * @author Daniela
+ *
+ */
+public class ProcessarFitxers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
