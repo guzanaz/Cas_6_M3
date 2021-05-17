f61ca9e0a27b8230dbd519247775b6d1a4b45bad
Daniela <daniela.gallardo.reyes@gmail.com>
2021-05-17 18:18:28 +0200

    update
    comments

--- Desenvolupament/src/pkgGestioFitxersInstitut/ProcessarFitxers.java
+++ Desenvolupament/src/pkgGestioFitxersInstitut/ProcessarFitxers.java
@@ -2,16 +2,19 @@
  * 
  */
 package pkgGestioFitxersInstitut;
-
 import java.io.File;
 import java.util.Scanner;
 
 /**
- * @author Daniela
- *
+ * Módulo ProcessarFitxers.
+ * Pide la ruta de los ficheros a procesar.
+ * Procesa todos los ficheros de un directorio.
+ * Crea un archivo nuevo con la siguiente información de los PCs: Aula, PC, Marca, Modelo, Número de serie, Mac Wifi, Mac ethernet.
+ * @author Daniela Gallardo Reyes
+ * @version 1.0 (entrega express)
+ * @since 17-05-2021
  */
 public class ProcessarFitxers{
-
 	/**
 	 * @param args
 	 */
--- Desenvolupament/src/pkgGestioFitxersInstitut/VolcatDeDades.java
+++ Desenvolupament/src/pkgGestioFitxersInstitut/VolcatDeDades.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 import java.io.*;
 
 /**
- * @author Daniela
- *
+ * Cas_6 M03 VolcatDeDades: Programa inicial del pkgGestioFitxersInstitut. 
+ * Implementa una aplicación para la gestión de la información de los PCs del Institut Montsia a través de los módulos:
+ * ProcessarFitxers.java
+ * IntroduirManualmentInfo.java
+ * ModificarInfoPC.java
+ * EsborrarInfoPC.java
+ * ConsultarPCsPerAula.java
+ * También Utiliza la librería java.io
+ * @author Daniela Gallardo Reyes
+ * @version 1.0 (entrega express)
+ * @since 17-05-2021
  */
 
-public class volcatDeDades {
+public class VolcatDeDades {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
