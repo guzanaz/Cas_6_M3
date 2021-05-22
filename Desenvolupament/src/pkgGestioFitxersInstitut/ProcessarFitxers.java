f1709209bd0a2994e71260663c05feab4254076b
Daniela <daniela.gallardo.reyes@gmail.com>
2021-05-22 14:28:58 +0200

    update
    pruebas

--- Desenvolupament/src/pkgGestioFitxersInstitut/ProcessarFitxers.java
+++ Desenvolupament/src/pkgGestioFitxersInstitut/ProcessarFitxers.java
@@ -2,36 +2,81 @@
  * 
  */
 package pkgGestioFitxersInstitut;
+
 import java.io.File;
 import java.util.Scanner;
 
 /**
- * Módulo ProcessarFitxers.
- * Pide la ruta de los ficheros a procesar.
- * Procesa todos los ficheros de un directorio.
- * Crea un archivo nuevo con la siguiente información de los PCs: Aula, PC, Marca, Modelo, Número de serie, Mac Wifi, Mac ethernet.
+ * Módulo ProcessarFitxers. Pide la ruta de los ficheros a procesar. Procesa
+ * todos los ficheros de un directorio. Crea un archivo nuevo con la siguiente
+ * información de los PCs: Aula, PC, Marca, Modelo, Número de serie, Mac Wifi,
+ * Mac ethernet.
+ * 
  * @author Daniela Gallardo Reyes
  * @version 1.0 (entrega express)
  * @since 17-05-2021
  */
-public class ProcessarFitxers{
+public class ProcessarFitxers {
+	private static File f;
+	private static String nomfitxer;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola ");
-		File f = new File("/Users/Daniela/eclipse-workspace/comprova_portatils_feb2021/dmidecode/a22_pc03 - Maria Merino Sanjuán.txt");
-		try {
-			Scanner lector = new Scanner(f);
+		f = obtenerArchivo();
+		leer(f);
+	}
 
-			while (lector.hasNext()) {
-				String s = lector.nextLine();
-				System.out.println(s);
+	/**
+	 * Method obtenerArchivo. Para obtener el archivo. Pide ingresar por teclado la
+	 * ruta del objeto File al que se desea acceder.
+	 * 
+	 * @param empty.
+	 * @return a File object.
+	 */
+	public static File obtenerArchivo() {
+		File nuevoArchivo;
+		String path;
+		Scanner sc;
+		System.out.println("Escriu la ruta de l'arxiu que vols processar");
+		sc = new Scanner(System.in);
+		path = sc.nextLine();
+		nuevoArchivo = new File(path);
+		sc.close();
+		return nuevoArchivo;
+	}
+
+	/**
+	 * Method leer. Lee un archivo y filtra información específica.
+	 * 
+	 * @param empty.
+	 * @return
+	 */
+	public static void leer(File f2) {
+		try {
+			nomfitxer = f2.getName();
+			// prints
+			System.out.println(nomfitxer);
+			Scanner sc = new Scanner(f2);
+			while (sc.hasNext()) {
+				String s = sc.nextLine();
+				// imprimir los campos de la marca y modelo
+				if ((s.contains("Product Name")) || //dmidecode 
+						(s.contains("producto")) ||//lshw
+						(s.contains("vendor")) || //lshw
+						(s.contains("Manufacturer")) || //dmidecode 
+						(s.contains("serial")) ||//lshw
+						(s.contains("Serial Number"))) { //dmidecode 
+					System.out.println(s);
+					// evitar que imprima información de placa base y posteriores
+				}else if(s.contentEquals("Base Board Information")){
+					System.exit(0);
+				}
 			}
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
 	}
-
 }
--- Desenvolupament/src/pkgGestioFitxersInstitut/VolcatDeDades.java
+++ Desenvolupament/src/pkgGestioFitxersInstitut/VolcatDeDades.java
@@ -3,35 +3,77 @@ import java.util.Scanner;
 import java.io.*;
 
 /**
- * Cas_6 M03 VolcatDeDades: Programa inicial del pkgGestioFitxersInstitut. 
- * Implementa una aplicación para la gestión de la información de los PCs del Institut Montsia a través de los módulos:
- * ProcessarFitxers.java
- * IntroduirManualmentInfo.java
- * ModificarInfoPC.java
- * EsborrarInfoPC.java
- * ConsultarPCsPerAula.java
- * También Utiliza la librería java.io
+ * Cas_6 M03 VolcatDeDades: Programa inicial del pkgGestioFitxersInstitut.
+ * Implementa una aplicación para la gestión de la información de los PCs del
+ * Institut Montsia a través de los módulos: ProcessarFitxers.java
+ * IntroduirManualmentInfo.java ModificarInfoPC.java EsborrarInfoPC.java
+ * ConsultarPCsPerAula.java También Utiliza la librería java.io
+ * 
  * @author Daniela Gallardo Reyes
  * @version 1.0 (entrega express)
  * @since 17-05-2021
  */
 
 public class VolcatDeDades {
-	
+	// main
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hola ");
-				File f= new File("/Users/Daniela/eclipse-workspace/comprova_portatils_feb2021/dmidecode/a22_pc03 - Maria Merino Sanjuán.txt");
-		try {
-			Scanner lector= new Scanner(f);
-			
-			while (lector.hasNext()) {
-				String s= lector.nextLine();
-				System.out.println(s);
+		// 1.creamos el menú de opciones
+		String[] opcions = new String[6];
+		Scanner sc = new Scanner(System.in);
+		opcions[0] = "[p] Processar Fitxers";
+		opcions[1] = "[i] Introduir Informació";
+		opcions[2] = "[m] Modificar Informació d'un PC";
+		opcions[3] = "[e] Esborrar Informació d'un PC";
+		opcions[4] = "[c] Consultar PCs per Aula";
+		opcions[5] = "[x] Sortir";
+
+		// 2. variable para salir del programa
+		boolean sortir = false;
+
+		do {
+			// var para guardarnos la opcion seleccionada
+			char opcio;
+			// llamamos al metodo para imprimir el menu
+			opcio = menu(opcions);
+
+			switch (opcio) {
+
+			case 'p':
+				System.out.println("----------------------------------");
+				System.out.println("       [p] Processar Fitxers      ");
+				System.out.println("----------------------------------");
+
+				break;
 			}
+		} while (sortir == false);
+	}// fin del main
+
+	/**
+	 * Función Menú. Muestra el menú de opciones por pantalla y pide ingresar una
+	 * opción del menú por teclado
+	 * 
+	 * @param opcions
+	 * @return variable tipo char (opción seleccionada)
+	 */
+	public static char menu(String[] opcions) {
+		System.out.println("----------------------------------");
+		System.out.println("|              MENÚ              |");
+		System.out.println("----------------------------------");
+		for (int i = 0; i < opcions.length; i++) {
+			System.out.println(opcions[i]);
 		}
-		catch (Exception e) {
-			System.out.println(e.toString());
-		}
+		// Preguntamos qué opción seleccionarán
+		System.out.println("-----------------------------");
+		System.out.println("|       QUÉ VOLS FER?       |");
+		System.out.println("-----------------------------");
+		System.out.println("    [ingressa una opció]   ");
+
+		// entrada por teclado
+		Scanner sc = new Scanner(System.in);
+		// guardamos la entrada en opcio
+		char opcio1 = sc.next().charAt(0);
+		// retornamos opcio al main
+		return opcio1;
+
 	}
 }
