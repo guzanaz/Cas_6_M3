e12e50304134c9f54fc217dca057a1234cb14925
Daniela <daniela.gallardo.reyes@gmail.com>
2021-05-23 11:56:43 +0200

    Update ProcessarFitxers.java
    Método LeerFiltrar: cuando se entrega la ruta de un objeto tipo file extensión .txt (no un directorio) puede extraer el Aula, nro. de PC, Marca, modelo y nro. de serie.
    
    Queda pendiente la dirección MAC wifi y MAC ethernet

--- Desenvolupament/src/pkgGestioFitxersInstitut/ProcessarFitxers.java
+++ Desenvolupament/src/pkgGestioFitxersInstitut/ProcessarFitxers.java
@@ -4,8 +4,7 @@
 package pkgGestioFitxersInstitut;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -57,74 +56,108 @@ public class ProcessarFitxers {
 	 * @return
 	 */
 	public static void leerFiltrar(File f2) {
+		String Parent="";
 		try {
 			nomfitxer = f2.getName();
 			// función para obtener número de Aula y número de PC
 			nomAulaPC(nomfitxer);
-			//procesar texto
+			// procesar texto
 			Scanner sc = new Scanner(f2);
-			
-			while (sc.hasNext()) {
-				String s = sc.nextLine();
-				//método para obtener marca
-				getMarca(f2,s);
-				// imprimir los campos de la marca y modelo
-				if ((s.contains("Product Name")) || // dmidecode
-						(s.contains("producto")) || // lshw
-						(s.contains("vendor")) || // lshw
-						(s.contains("Manufacturer")) || // dmidecode
-						(s.contains("serial")) || // lshw
-						(s.contains("Serial Number"))) { // dmidecode
-					System.out.println(s);
-					// evitar que imprima información de placa base y posteriores
-				} else if (s.contentEquals("Base Board Information")) {
-					System.exit(0);
+			// si el nombre del fichero contiene ls
+			if (nomfitxer.contains("ls")) {
+				while (sc.hasNext()) {
+					String lector = sc.nextLine();
+					// condiciones para rescatar modelo, marca y nro. de serie
+					if ((lector.contains("producto")) || (lector.contains("product"))) {
+						String[] lineaen2partes = lector.split(":");
+						String parametro = lineaen2partes[1];
+						// imprimir modelo del ordenador
+						System.out.println("Modelo: " + parametro);
+
+					} else if ((lector.contains("vendor")) || (lector.contains("fabricante"))) {
+						String[] lineaen2partes = lector.split(":");
+						String parametro = lineaen2partes[1];
+						// imprimir marca del ordenador
+						System.out.println("Marca: " + parametro);
+					} else if ((lector.contains("serie")) || (lector.contains("serial"))) {
+						String[] lineaen2partes = lector.split(":");
+						String parametro = lineaen2partes[1];
+						// imprimir nro. serie del ordenador
+						System.out.println("Nro. de Serie: " + parametro);
+					}
+					else if ((lector.contains("width")) || (lector.contains("anchura"))) {
+						break;
+					}
+					
+					//condiciones para sacar dirección mac y wifi cuando sea la carpeta lshw
+					
+					//imprimir la dirección mac y wifi cuando sea la carpeta lshw
+				}
+					
+			} else if((f2.getParent()).contains("dmidecode")){
+				while (sc.hasNext()) {
+					String lector = sc.nextLine();
+					// condiciones para rescatar modelo, marca y nro. de serie
+					if (lector.contains("Manufacturer")) {
+						String[] lineaen2partes = lector.split(":");
+						String marca = lineaen2partes[1];
+						// imprimir marca del ordenador
+						System.out.println("Marca: " + marca);
+
+					} else if(lector.contains("Product Name")) {
+						String[] lineaen2partes = lector.split(":");
+						String modelo = lineaen2partes[1];
+						// imprimir marca del ordenador
+						System.out.println("Modelo: " + modelo);
+					} else if (lector.contains("Serial Number")){
+						String[] lineaen2partes = lector.split(":");
+						String nroSerie = lineaen2partes[1];
+						// imprimir nro. serie del ordenador
+						System.out.println("Nro. de Serie: " + nroSerie);
+					}
+					else if (lector.contains("UUID")){
+						break;
+					}
+		
+					//condiciones para sacar dirección mac y wifi cuando sea la carpeta dmidecode
+					//imprimir la dirección mac y wifi cuando sea la carpeta dmidecode
 				}
 			}
-		} catch (Exception e) {
+		} catch (IOException e) {
 			System.out.println(e.toString());
 		}
 	}
 
-	private static void getMarca(File f2,String s) {
-		System.out.println(f2.getParent());			
-		
-		
-	}
-
 	/**
-	 * Method nomAulaPC. 
-	 * Para obtener e imprimir número de aula y de ordenador.
+	 * Method nomAulaPC. Para obtener e imprimir número de aula y de ordenador.
+	 * 
 	 * @param String nomfitxer2.
 	 * @return res.
 	 */
 	private static void nomAulaPC(String nomfitxer2) {
 		System.out.print("AULA ");
-			// si está en la carpeta de la consulta lshw...
-			if ((nomfitxer2.charAt(0) == 'l') && (nomfitxer.charAt(1) == 's')) {
-				for(int i=4;i<=5;i++) {
-					// imprime el nro de aula así:
-					System.out.print(nomfitxer2.charAt(i));
-				}
-					System.out.print(" | ");
-				for(int i=7;i<=10;i++)	
-					System.out.print(nomfitxer2.charAt(i));
-				}//si está en la carpeta de la consulta dmidecode...
-			else if((nomfitxer2.charAt(0) == 'A')||(nomfitxer.charAt(0)=='a')) {
+		// si está en la carpeta de la consulta lshw...
+		if ((nomfitxer2.charAt(0) == 'l') && (nomfitxer.charAt(1) == 's')) {
+			for (int i = 4; i <= 5; i++) {
 				// imprime el nro de aula así:
-				for (int i=1;i<=2;i++) {
-					System.out.print(nomfitxer2.charAt(i));
-				}
-				System.out.print(" | ");
-				for (int i=4; i<=7;i++) {
-					System.out.print(nomfitxer2.charAt(i));
-				}
+				System.out.print(nomfitxer2.charAt(i));
 			}
+			System.out.print(" | ");
+			for (int i = 7; i <= 10; i++)
+				System.out.print(nomfitxer2.charAt(i));
+		} // si está en la carpeta de la consulta dmidecode...
+		else if ((nomfitxer2.charAt(0) == 'A') || (nomfitxer.charAt(0) == 'a')) {
+			// imprime el nro de aula así:
+			for (int i = 1; i <= 2; i++) {
+				System.out.print(nomfitxer2.charAt(i));
+			}
+			System.out.print(" | ");
+			for (int i = 4; i <= 7; i++) {
+				System.out.print(nomfitxer2.charAt(i));
+			}
+		}
 		System.out.println(" ");// Salto de línea
 		return;
 	}
-	
-	
-	
-	
+
 }
